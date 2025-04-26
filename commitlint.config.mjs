0532303e12c262c1bcb1ba57ca68b73242ef9630
nickxsy <104327959+nickozy@users.noreply.github.com>
2025-04-27 00:22:45 +0300

    test: test

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,11 +1,41 @@
 export default {
   parserPreset: 'conventional-changelog-conventionalcommits',
   extends: ['@commitlint/config-conventional'],
-  rules: {},
+  rules: {
+    'body-leading-blank': [1, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [1, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
+    ],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test'
+      ]
+    ]
+  },
   prompt: {
-    settings: {
-      emoji: true
-    },
+    settings: {},
     messages: {
       skip: ':skip',
       max: 'upper %d chars',
